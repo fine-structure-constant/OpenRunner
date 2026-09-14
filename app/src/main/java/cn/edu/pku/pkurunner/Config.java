a7baa062642889eb3fa66cf0f70a3d87c3af6a81
dfshfghj <2400011461@stu.pku.edu.cn>
2026-09-15 03:44:29 +0800

    chore: update feedback

--- app/src/main/java/cn/edu/pku/pkurunner/Config.java
+++ app/src/main/java/cn/edu/pku/pkurunner/Config.java
@@ -2,6 +2,6 @@ package cn.edu.pku.pkurunner;
 
 public final class Config {
     public static final String APK_URL = "https://pkunewyouth.pku.edu.cn/public/apks/pkurunner-latest.apk";
-    public static final String FEEDBACK_URL = "https://pku-runner.github.io";
+    public static final String FEEDBACK_URL = "https://github.com/dfshfghj/OpenRunner/issues";
     public static final String OFFLINE_APK_URL = "https://github.com/pku-runner/pku-runner.github.io/releases/latest";
 }
